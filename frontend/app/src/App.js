e598d71192fa9525563f6a1bed85d2eb524b3e78
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-07-02 11:28:31 +0300

    Page structuree

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -3,20 +3,21 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.scss';
 
-import { updateStaticContent } from './redux/staticcontent/actions';
-import * as ContentSelectors from './redux/staticcontent/selectors';
-
 import Header from './components/Header';
-import HeroImage from './components/HeroImage';
-import HeroCTA from './components/HeroCTA';
 import EditorTools from './components/EditorTools';
-import BasicSection from './components/BasicSection';
-import Markdown from './components/Markdown';
-import Divider from './components/Divider';
-import StatBlocks from './components/StatBlocks';
 
-import TracksGrid from './components/TracksGrid';
-import SingleColumnSection from './components/SingleColumnSection';
+import HomePage from './Pages/Home';
+import Page2 from './Pages/Home';
+
+import * as StaticContentActions from './redux/staticcontent/actions';
+
+const NotFound = () => {
+    return (
+        <div style={{ marginTop: '100px' }}>
+            <h1>Not Found</h1>
+        </div>
+    );
+};
 
 class App extends Component {
     async componentDidMount() {
@@ -24,70 +25,17 @@ class App extends Component {
     }
 
     render() {
-        const { getText, getMedia } = this.props;
         return (
-            <Router className="foooofoo">
+            <Router>
                 <div className="App">
                     <Header />
                     <main className="App--main">
-                        <HeroImage image={getMedia('homePageHeaderImage')}>
-                            <HeroCTA
-                                image={getMedia('homePageHeroCtaLogo')}
-                                subtitle={getText('homePageHeroCtaSubtitle')}
-                                ctaText={getText('homePageHeroCtaText')}
-                                ctaLink={getText('homePageHeroCtaLink')}
-                            />
-                        </HeroImage>
-                        <button className="App--main-applyButton">
-                            {getText('homePageApplyButton')}
-                        </button>
-                        <Divider lg />
-                        <div className="App--main-buttons">
-                            <button className="App--main-infoButton">
-                                {getText('homePageInfoButton')}
-                            </button>
-                            <button className="App--main-volunteerButton">
-                                {getText('homePageVolunteerButton')}
-                            </button>
-                        </div>
-                        <Divider md />
-                        <SingleColumnSection
-                            title={getText('junctionComingSlogan')}
-                        />
+                        <Switch>
+                            <Route exact path="/" component={HomePage} />
+                            <Route exact path="/other-page" component={Page2} />
 
-                        <BasicSection
-                            title={getText('junctionMainTitle')}
-                            subtitle={getText('junctionMainSubtitle')}
-                        >
-                            <span className="App--main-introText">
-                                {getText('homePageIntroText')}
-                            </span>
-                            <StatBlocks />
-                        </BasicSection>
-                        <Divider md />
-                        <BasicSection
-                            title={getText('homePageJourneyTitle')}
-                            subtitle={getText('homePageJourneySubtitle')}
-                        />
-                        <Divider lg />
-                        <SingleColumnSection
-                            title={getText('tracksTitle')}
-                            subtitle={getText('tracksSubtitle')}
-                        />
-                        <TracksGrid />
-                        <Divider md />
-                        <SingleColumnSection>
-                            <div className="App--main-videoContainer">
-                                <iframe
-                                    className="App--main-videoContainer-video"
-                                    src={getText('homePageVideoLink')}
-                                    frameborder="0"
-                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                    allowfullscreen
-                                />
-                            </div>
-                        </SingleColumnSection>
-                        <Divider lg />
+                            <Route component={NotFound} />
+                        </Switch>
                     </main>
                     <EditorTools />
                 </div>
@@ -96,14 +44,7 @@ class App extends Component {
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        getText: ContentSelectors.buildGetText(state),
-        getMedia: ContentSelectors.buildGetMedia(state)
-    };
-};
-
 export default connect(
-    mapStateToProps,
-    { updateStaticContent }
+    null,
+    { updateStaticContent: StaticContentActions.updateStaticContent }
 )(App);
--- frontend/app/src/Pages/Home/index.js
+++ frontend/app/src/Pages/Home/index.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import TracksGrid from '../../components/TracksGrid';
-import HeaderImage from '../../components/HeaderImage';
 import './style.scss';
+
+import { updateStaticContent } from '../../redux/staticcontent/actions';
+import * as ContentSelectors from '../../redux/staticcontent/selectors';
+
+import HeroImage from '../../components/HeroImage';
+import HeroCTA from '../../components/HeroCTA';
+import BasicSection from '../../components/BasicSection';
+import Markdown from '../../components/Markdown';
+import Divider from '../../components/Divider';
+import StatBlocks from '../../components/StatBlocks';
+import TracksGrid from '../../components/TracksGrid';
+import SingleColumnSection from '../../components/SingleColumnSection';
+import Page from '../PageHOC';
 
 class HomePage extends Component {
     render() {
-        return <button className="Page--appy-button" />;
+        const { getMedia, getText } = this.props;
+
+        return (
+            <Page className="HomePage">
+                <HeroImage image={getMedia('homePageHeaderImage')}>
+                    <HeroCTA
+                        image={getMedia('homePageHeroCtaLogo')}
+                        subtitle={getText('homePageHeroCtaSubtitle')}
+                        ctaText={getText('homePageHeroCtaText')}
+                        ctaLink={getText('homePageHeroCtaLink')}
+                    />
+                </HeroImage>
+                <button className="App--main-applyButton">{getText('homePageApplyButton')}</button>
+                <Divider lg />
+                <div className="App--main-buttons">
+                    <button className="App--main-infoButton">{getText('homePageInfoButton')}</button>
+                    <button className="App--main-volunteerButton">{getText('homePageVolunteerButton')}</button>
+                </div>
+                <Divider md />
+                <SingleColumnSection title={getText('junctionComingSlogan')} />
+
+                <BasicSection title={getText('junctionMainTitle')} subtitle={getText('junctionMainSubtitle')}>
+                    <span className="App--main-introText">{getText('homePageIntroText')}</span>
+                    <StatBlocks />
+                </BasicSection>
+                <Divider md />
+                <BasicSection title={getText('homePageJourneyTitle')} subtitle={getText('homePageJourneySubtitle')} />
+                <Divider lg />
+                <SingleColumnSection title={getText('tracksTitle')} subtitle={getText('tracksSubtitle')} />
+                <TracksGrid />
+                <Divider md />
+                <SingleColumnSection>
+                    <Markdown source={getText('homePageVideoLink')} />
+                </SingleColumnSection>
+                <Divider lg />
+            </Page>
+        );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        getText: ContentSelectors.buildGetText(state),
+        getMedia: ContentSelectors.buildGetMedia(state)
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    { updateStaticContent }
+)(HomePage);
