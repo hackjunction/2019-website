fc9474b72c6a405f0b5d96364b55afc1d61cee9f
Laukkanen Leevi <s1700530@student.businesscollege.fi>
2019-07-05 15:16:48 +0300

    fixed config

--- frontend/app/src/config.js
+++ frontend/app/src/config.js
@@ -1,10 +1,9 @@
 const config = {
-    /*CLOUDINARY_CLOUD_NAME: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || '',
-    GRAPHQL_BASE_URL: process.env.REACT_APP_GRAPHQL_BASE_URL || '',*/
+    //CLOUDINARY_CLOUD_NAME: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_CLOUD_NAME: 'dwvybqdgo',
-    API_BASE_URL: '	https://api.cloudinary.com/v1_1/dwvybqdgo',
-    GRAPHQL_BASE_URL: 'http://res.cloudinary.com/dwvybqdgo',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || '',
+    GRAPHQL_BASE_URL: process.env.REACT_APP_GRAPHQL_BASE_URL || '',
+
     IS_DEV: process.env.NODE_ENV === 'development',
     /**
      * If IS_DEBUG is set to true, the page will show some content editor tools
