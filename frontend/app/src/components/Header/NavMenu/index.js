2ccfe745f55fcdcb6d644429f42964b7c8e6d14a
Laukkanen Leevi <s1700530@student.businesscollege.fi>
2019-07-24 14:21:12 +0300

    Added link to main website in header NavMenu

--- frontend/app/src/components/Header/NavMenu/index.js
+++ frontend/app/src/components/Header/NavMenu/index.js
@@ -4,16 +4,21 @@ import './style.scss';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import * as StaticSelectors from '../../../redux/staticcontent/selectors';
+
 import { toggleSidebar } from '../../../redux/dynamiccontent/actions';
 import { isSidebarOpen } from '../../../redux/dynamiccontent/selectors';
 
-const NavMenuInner = memo(({ filler }) => {
+import Image from '../../Image';
+import Divider from '../../Divider';
+
+const NavMenuInner = memo(({ getMedia } = this.props) => {
     return (
         <div className="NavMenu--inner">
             <Link to="/">
-                <img
+                <Image
                     className="NavMenu--inner__logo"
-                    src={require('../../../assets/logos/wordmark_black.png')}
+                    image={getMedia('navMenuTopLogo')}
                     alt="Junction text logo"
                 />
             </Link>
@@ -32,21 +37,31 @@ const NavMenuInner = memo(({ filler }) => {
                 </Link>
 
                 <h6 className="NavMenu--inner__menu-title">Info</h6>
-                <Link
-                    className="NavMenu--inner__menu-item"
-                    to="/challenges"
-                >
+                <Link className="NavMenu--inner__menu-item" to="/challenges">
                     Tracks & Challenges
                 </Link>
                 <Link className="NavMenu--inner__menu-item" to="/volunteer">
                     Volunteer Info
                 </Link>
             </nav>
+            <Divider sm />
+            <h6 className="NavMenu--inner__menu-title">
+                Junction main website
+            </h6>
+            <a href="https://hackjunction.com">
+                <Image
+                    className="NavMenu--bottom-logo"
+                    image={getMedia('navMenuBottomLogo')}
+                    alt="Junction logo"
+                />
+            </a>
         </div>
     );
 });
 
-const mapStateToPropsInner = state => ({});
+const mapStateToPropsInner = state => ({
+    getMedia: StaticSelectors.buildGetMedia(state)
+});
 
 const ConnectedNavMenuInner = connect(mapStateToPropsInner)(NavMenuInner);
 
