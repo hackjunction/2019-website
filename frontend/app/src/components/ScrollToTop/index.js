62a43f8e2ca577d9401aadbfb888db983bbc70ce
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-08-01 16:36:20 +0300

    fix scrolltoTop

--- frontend/app/src/components/ScrollToTop/index.js
+++ frontend/app/src/components/ScrollToTop/index.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router';
 
 class ScrollToTop extends React.Component {
     componentDidUpdate(prevProps) {
-        if (this.props.location.pathname !== prevProps.location.pathname) {
+        if (this.props.location.pathname.split('#')[0] !== prevProps.location.pathname.split('#')[0]) {
             window.scrollTo(0, 0);
         }
     }
